c61dc972b6fe08439ad36f9ae3915b07b2fba627
casper9474 <casper94749474@gmail.com>
2026-04-30 13:47:07 +0200

    refactor: move entry point to parent package

--- src/main/java/pl/polsl/league/SoccerLeagueApplication.java
+++ src/main/java/pl/polsl/league/SoccerLeagueApplication.java
@@ -1,4 +1,4 @@
-package pl.polsl.league.app;
+package pl.polsl.league;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/pl/polsl/league/SoccerLeagueApplicationTests.java
+++ src/test/java/pl/polsl/league/SoccerLeagueApplicationTests.java
@@ -1,4 +1,4 @@
-package pl.polsl.league.app;
+package pl.polsl.league;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
