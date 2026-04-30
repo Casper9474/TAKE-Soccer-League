22764793c081325f3aabbcab1c2dd384c9e6dae3
casper9474 <casper94749474@gmail.com>
2026-04-30 13:26:01 +0200

    Initial commit

---
+++ src/test/java/pl/polsl/league/app/SoccerLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.league.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
